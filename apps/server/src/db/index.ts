cbcebb28d21c9fbda850af56c1324cbc45b8a4c0
hermani <hermani457@gmail.com>
2026-01-17 16:18:14 -0300

    feat: Configure Neon client to use `ws` as its WebSocket constructor.

--- apps/server/src/db/index.ts
+++ apps/server/src/db/index.ts
@@ -1,6 +1,9 @@
-import { Pool } from '@neondatabase/serverless';
+import { Pool, neonConfig } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-serverless';
+import ws from "ws";
 import * as schema from "./shared/index.js";
+
+neonConfig.webSocketConstructor = ws;
 
 import "dotenv/config";
 
