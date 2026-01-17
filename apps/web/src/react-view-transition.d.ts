3fed36087a1412a311bbc5dded6127cb47d88c9f
diego <hermani457@gmail.com>
2026-01-17 03:03:42 -0300

    remove unused component

--- apps/web/src/react-view-transition.d.ts
+++
@@ -1,8 +0,0 @@
-declare module "react" {
-  export interface ViewTransitionProps {
-    children?: React.ReactNode;
-    name?: string;
-  }
-
-  export const ViewTransition: React.ComponentType<ViewTransitionProps>;
-}
